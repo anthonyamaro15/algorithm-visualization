86a24a472828f864f2f71efcf533940224a4ac1c
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-10-11 13:31:04 -0700

    implemented mergeSort algorithm

--- src/components/SortingAlgo.js
+++ src/components/SortingAlgo.js
@@ -10,18 +10,59 @@ const SortingAlgo = () => {
   const createArrays = () => {
     const arr = [];
     for (let i = 0; i < 350; i++) {
-      arr.push(Math.floor(Math.random() * (1000 - 5 + 1) + 5));
+      arr.push(Math.floor(Math.random() * (700 - 5 + 1) + 5));
     }
     setArray(arr);
   };
 
-  console.log("what are therey ", array);
+  const merge = (arr1, arr2) => {
+    let result = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+      if (arr2[j] > arr1[i]) {
+        result.push(arr1[i]);
+        i++;
+      } else {
+        result.push(arr2[j]);
+        j++;
+      }
+    }
+    while (i < arr1.length) {
+      result.push(arr1[i]);
+      i++;
+    }
+
+    while (j < arr2.length) {
+      result.push(arr2[j]);
+      j++;
+    }
+
+    return result;
+  };
+
+  const mergeSort = (arr) => {
+    if (arr.length <= 1) {
+      return arr;
+    }
+    let middle = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, middle));
+    let right = mergeSort(arr.slice(middle));
+    return merge(left, right);
+  };
+
+  const mergeSortFunc = () => {
+    let sortArr = mergeSort(array);
+    setArray(sortArr);
+  };
 
   return (
     <div className="SortingAlgo">
       {array.map((values, i) => (
         <div className="bars" key={i} style={{ height: `${values}px` }}></div>
       ))}
+      <button onClick={mergeSortFunc}>merge sort</button>
     </div>
   );
 };
